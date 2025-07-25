a95e1a1d798f8818a575ae81c55748e3eb65a4d1
Thomas James <wjamesthomas3@gmail.com>
2025-07-24 20:50:02 -0400

    9: Add more static content

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from functions.generate import copy_static, copy_dir_recursive, generate_page
 
 def main():
+    print("Copying static files...")
+    copy_static("static", "public")
     print("Generating index.html...")
     generate_page("content/index.md", "template.html", "public/index.html")
 
